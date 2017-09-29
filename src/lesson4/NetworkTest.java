80254c2a1116058e30ecdf69cc27c8ad0def0f11
JiWon <jw0712@skku.edu>
2017-09-29 14:36:41 +0900

    내부클래스 기초와 중첩클래스의 응용

--- src/lesson4/NetworkTest.java
+++ src/lesson4/NetworkTest.java
@@ -1,12 +1,26 @@
 package lesson4;
 
 import lesson4.network.Network;
+import lesson4.network.Network2;
 
 public class NetworkTest {
     static public void main(String[] arg){
         Network net1 = new Network();
+        Network net2 = new Network();
         //Network.Member member1 = new Network.Member("fred");
-        Network.Member fredblahblahblach = net1.enroll("fred");
+        Network.Member fredblahblah = net1.enroll("fred");
+
+        System.out.println((fredblahblah.belongTo(net2)));
+        Network.Member jane = net1.new Member("jane");
+        Network.Member tom = net2.new Member("tom");
+
+        Network2 n1 = new Network2();
+        Network2 n2 = new Network2();
+        Network2.Member a = n1.enroll("a");
+        a.leave();
+        Network2.Member b = new Network2.Member(n1, "b");
+
 
     }
+
 }
--- src/lesson4/network/Network.java
+++ src/lesson4/network/Network.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Network {
     public class Member{
 
-        final.private String name; //한번 선언하면 절대 바뀌지 않는다 이름 못바꿔요!
+        final private String name; //한번 선언하면 절대 바뀌지 않는다 이름 못바꿔요!
         private ArrayList<Member> friends; //원소가 되는 아이의 클래스 이름을 <>안에 넣어준다
 
         public Member(String name){
@@ -13,11 +13,12 @@ public class Network {
             this.friends = new ArrayList<>();
 
         }
-
+        public void leave(){ //여기까진 ok
+            //members.remove(this); 캡슐화를 어겼으므로 Network의 내장을 Member가 뜯어보는 것. Member는 Network의 소속일 뿐...!! 어떻게 remove하는지는 몰라야 함 멤버스가..
+            //unenroll(this); //캡슐화 **
         }
-        public void leave(){
-            members.remove(this);
-
+        public boolean belongTo(Network n){
+            return n == Network.this;
         }
     }
     //Network-------------------------------------------------------------
@@ -29,7 +30,7 @@ public class Network {
         members.add(newMember);
         return newMember;
     }
-    public void remove(Member member){
+    public void unenroll(Member member){
         members.remove(member);
     }
 }
